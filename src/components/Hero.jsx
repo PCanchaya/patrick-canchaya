a835b762d520cdf71daac6a3dbef0540138487a7
Patrick Canchaya <canchay4@gmail.com>
2025-09-29 17:20:23 +0700

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -133,7 +133,7 @@ const Hero = () => {
 
                 <div className="absolute left-0 top-0 z-40 size-full">
             <div className="mt-24 px-5 sm:px-10">
-                <h1 className="special-font hero-heading text-blue-100">redefi<b>n</b>e</h1>
+                <h1 className="special-font hero-heading text-blue-100">Patri<b>c</b>k</h1>
 
                 <p className="mb-5 max-w-64 font-robert-regular text-blue-100"> Enter the Metagame Layer <br /> Unleash the PLay Economy</p>
 
@@ -142,7 +142,7 @@ const Hero = () => {
                 </div>
             </div>
             <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
-                G<b>a</b>ming
+                C<b>a</b>nchaya
             </h1>
 
         </div>
