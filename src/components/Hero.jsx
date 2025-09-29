9386d3ba77f25f1f6dc3410cd44af6a07b050522
Patrick Canchaya <canchay4@gmail.com>
2025-09-29 17:24:23 +0700

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -128,14 +128,15 @@ const Hero = () => {
                 </div>
 
                 <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
-                    G<b>a</b>ming
+                    C<b>a</b>nchaya
                 </h1>
 
                 <div className="absolute left-0 top-0 z-40 size-full">
             <div className="mt-24 px-5 sm:px-10">
-                <h1 className="special-font hero-heading text-blue-100">Patri<b>c</b>k</h1>
+                <h1 className="special-font hero-heading
+                text-blue-100">Patri<b>c</b>k</h1>
 
-                <p className="mb-5 max-w-64 font-robert-regular text-blue-100"> Enter the Metagame Layer <br /> Unleash the PLay Economy</p>
+                <p className="mb-5 max-w-64 font-robert-regular text-blue-100"> Patricks <br /> Portfolio Website</p>
 
                 <Button id="watch-trailer" title="Watch Trailer" leftIcon={<TiLocationArrow />} containerClass="!bg-yellow-300 flex-center gap-1"/>
             </div>
